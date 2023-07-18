f26fa0bb330a6355df0d9336b99bbb0b5c96bf6a
odonok <122719073+odonok@users.noreply.github.com>
2023-07-18 14:16:25 +0900

    update 第3引数として結果ファイルのパスを追加

--- bisbislib/visualize/show_result.py
+++ bisbislib/visualize/show_result.py
@@ -1,4 +1,4 @@
 from typing import List
 
-def show_result(loss:List[float], accuracy:List[float])->None:
+def show_result(loss:List[float], accuracy:List[float], resultpath:str="graphs/result.png")->None:
     pass
