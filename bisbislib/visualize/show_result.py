9d15dede4d3647394da6f6d2b611be4c27645154
odonok <122719073+odonok@users.noreply.github.com>
2023-07-25 14:38:07 +0900

    update docstringの追加

--- bisbislib/visualize/show_result.py
+++ bisbislib/visualize/show_result.py
@@ -1,4 +1,12 @@
 from typing import List
 
 def show_result(loss:List[float], accuracy:List[float], resultpath:str="graphs/result.png")->None:
-    pass
+    """得られた誤差や精度をグラフにして表示・保存する関数
+
+    Args:
+        loss(List[float]): 得られた誤差
+        accuracy(List[float]): 得られた精度
+        resultpath(str): 生成したグラフの保存先のフォルダ "default = graphs/result.png"
+    """
+    
+pass
--- test/visualize/test_show_result.py
+++ test/visualize/test_show_result.py
@@ -1,8 +1,7 @@
 import unittest
 import tempfile
+from bisbislib.visualize import show_result
 
 class TestShowResult(unittest.TestCase):
-    def test_show_result(
-        self
-    ):
-        
+    def test_show_result(self):
+        show_result
